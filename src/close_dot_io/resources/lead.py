79e75d660e994a7b255217fef89bc0b15f44f195
tristan-robinette <tristan-robinette@hotmail.ca>
2024-04-01 13:23:23 +0100

    allow any Contact model

--- src/close_dot_io/resources/lead.py
+++ src/close_dot_io/resources/lead.py
@@ -1,14 +1,18 @@
+from typing import TypeVar
+
 from pydantic import Field
 
 from .base import BaseResourceModel
 from .contact import Contact
 
+T = TypeVar("T", bound=Contact)
+
 
 class Lead(BaseResourceModel):
     name: str | None = Field(alias="display_name", default=None)
-    contacts: list[Contact] = []
+    contacts: list[T] = []
 
     @classmethod
-    def create_from_contact(cls, contact: Contact, **other_lead_data):
+    def create_from_contact(cls, contact: T, **other_lead_data):
         other_lead_data.pop("contacts", None)
         return cls(contacts=[contact], **other_lead_data)
