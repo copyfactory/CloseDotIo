150e55a31396394e853a9ce68e0484738e840c5f
tristan-robinette <tristan-robinette@hotmail.ca>
2024-05-31 10:53:15 +0100

    escape bad strings

--- src/close_dot_io/dict_util.py
+++ src/close_dot_io/dict_util.py
@@ -8,5 +8,6 @@ def replace_dynamic_fields(data: dict, dynamic_values: dict) -> dict:
     json_data = json.dumps(data)
     for key, value in dynamic_values.items():
         placeholder = "{{ " + key + " }}"
-        json_data = json_data.replace(placeholder, str(value))
+        escaped_value = json.dumps(value)[1:-1]
+        json_data = json_data.replace(placeholder, escaped_value)
     return json.loads(json_data)
